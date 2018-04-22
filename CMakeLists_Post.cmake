a9de3e2331bb7585d71277de824f5b2f2b9d6efc
Saad Khattak <saadrustam@gmail.com>
2018-04-22 19:42:57 -0400

    Minor changes
    Will still fail with travis. Need a better package manager.

--- CMakeLists_Post.cmake
+++ CMakeLists_Post.cmake
@@ -1,3 +1,5 @@
 # -----------------------------------------------------------------------------
 # user editable file that is added AFTER superbuild is completed. This file
 # can be used to add additional dependencies
+
+include(LocalVars.cmake OPTIONAL)
